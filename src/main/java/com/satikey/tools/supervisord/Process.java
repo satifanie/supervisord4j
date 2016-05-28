f5ffbd05a98aa90fee12eaef0b1f82f3e1048bc4
Lei Duan <duanlei@gdyd.com>
2016-05-28 11:55:25 +0800

    setup all api method

--- src/main/java/com/satikey/tools/supervisord/Process.java
+++ src/main/java/com/satikey/tools/supervisord/Process.java
@@ -18,4 +18,261 @@ public class Process {
 //'stdout_logfile': '/path/to/stdout-log',
 //'stderr_logfile': '/path/to/stderr-log',
 //'pid':            1}
+
+    /**
+     * Get info about a process named name
+     *
+     * @param processName name The name of the process (or ‘group:name’) @return struct result A
+     *                    structure containing data about the process
+     * @return string {@link http://supervisord.org/api.html#supervisor.rpcinterface.SupervisorNamespaceRPCInterface.getProcessInfo}
+     */
+    public String getProcessInfo(String processName) {
+        return null;
+    }
+
+    /**
+     * Get info about all processes
+     *
+     * @return array result An array of process status results
+     */
+    public String getAllProcessInfo() {
+        return null;
+    }
+
+    /**
+     * Start a process
+     *
+     * @param processName name Process name (or group:name, or group:*)
+     * @param waitToStart wait Wait for process to be fully started
+     * @return boolean result Always true unless error
+     */
+    public boolean startProcess(String processName, boolean waitToStart) {
+        return true;
+    }
+
+    /**
+     * Start all processes listed in the configuration file
+     *
+     * @param waitToStart wait Wait for each process to be fully started
+     * @return array result An array of process status info structs
+     */
+    public void startAllProcesses(boolean waitToStart) {
+    }
+
+    /**
+     * Start all processes in the group named ‘name’
+     *
+     * @param groupName   name The group name
+     * @param waitToStart wait Wait for each process to be fully started
+     * @return array result An array of process status info structs
+     */
+    public boolean startProcessGroup(String groupName, boolean waitToStart) {
+        return true;
+    }
+
+    /**
+     * Stop a process named by name
+     *
+     * @param processName name The name of the process to stop (or ‘group:name’)
+     * @param waitToStop  wait Wait for the process to be fully stopped
+     * @return boolean result Always return True unless error
+     */
+    public boolean stopProcess(String processName, boolean waitToStop) {
+        return true;
+    }
+
+    /**
+     * Stop all processes in the process group named ‘name’
+     *
+     * @param groupName  name The group name
+     * @param waitToStop wait Wait for each process to be fully stopped
+     * @return array result An array of process status info structs
+     */
+    public boolean stopProcessGroup(String groupName, boolean waitToStop) {
+        return true;
+    }
+
+    /**
+     * Stop all processes in the process list
+     *
+     * @param waitToStop wait Wait for each process to be fully stopped
+     * @return array result An array of process status info structs
+     */
+    public boolean stopAllProcesses(boolean waitToStop) {
+        return true;
+    }
+
+    /**
+     * Send an arbitrary UNIX signal to the process named by name
+     *
+     * @param processName name Name of the process to signal (or ‘group:name’)
+     * @param signal      signal Signal to send, as name (‘HUP’) or number (‘1’)
+     */
+    public boolean signalProcess(String processName, String signal) {
+        return true;
+    }
+
+    /**
+     * Send a signal to all processes in the group named ‘name’
+     *
+     * @param groupName name The group name
+     * @param signal    signal Signal to send, as name (‘HUP’) or number (‘1’)
+     * @return array
+     */
+    public boolean signalProcessGroup(String groupName, String signal) {
+        return true;
+    }
+
+
+    /**
+     * Send a signal to all processes in the process list
+     *
+     * @param signal signal Signal to send, as name (‘HUP’) or number (‘1’)
+     * @return array An array of process status info structs
+     */
+    public boolean signalAllProcesses(String signal) {
+        return true;
+    }
+
+    /**
+     * Send a string of chars to the stdin of the process name. If non-7-bit data is sent (unicode),
+     * it is encoded to utf-8 before being sent to the process’ stdin. If chars is not a string or
+     * is not unicode, raise INCORRECT_PARAMETERS. If the process is not running, raise NOT_RUNNING.
+     * If the process’ stdin cannot accept input (e.g. it was closed by the child process), raise
+     * NO_FILE.
+     *
+     * @param processName name The process name to send to (or ‘group:name’)
+     * @param chars       The character data to send to the process
+     * @return boolean result Always return True unless error
+     */
+    public boolean sendProcessStdin(String processName, String chars) {
+        return true;
+    }
+
+
+    /**
+     * Send an event that will be received by event listener subprocesses subscribing to the
+     * RemoteCommunicationEvent.
+     *
+     * @param type type String for the “type” key in the event header
+     * @param data Data for the event body
+     * @return boolean Always return True unless error
+     */
+    public boolean sendRemoteCommEvent(String type, String data) {
+        return true;
+    }
+
+    /**
+     * Reload configuration
+     *
+     * @return boolean result always return True unless error
+     */
+    public boolean reloadConfig() {
+        return true;
+    }
+
+
+    /**
+     * Update the config for a running process from config file.
+     *
+     * @param name name name of process group to add
+     * @return boolean result true if successful
+     */
+    public boolean addProcessGroup(String name) {
+        return true;
+    }
+
+
+    /**
+     * Remove a stopped process from the active configuration.
+     *
+     * @param name name of process group to remove
+     * @return boolean result Indicates whether the removal was successful
+     */
+    public boolean removeProcessGroup(String name) {
+        return true;
+    }
+
+    ////////////////////
+
+    /**
+     * Read length bytes from name’s stdout log starting at offset
+     *
+     * @param name   the name of the process (or ‘group:name’)
+     * @param offset offset to start reading from.
+     * @param length number of bytes to read from the log.
+     * @return string result Bytes of log
+     */
+    public boolean readProcessStdoutLog(String name, int offset, int length) {
+        return true;
+    }
+
+    /**
+     * Read length bytes from name’s stderr log starting at offset
+     *
+     * @param name   the name of the process (or ‘group:name’)
+     * @param offset offset to start reading from.
+     * @param length number of bytes to read from the log.
+     * @return string result Bytes of log
+     */
+    public boolean readProcessStderrLog(String name, int offset, int length) {
+        return true;
+    }
+
+    /**
+     * Provides a more efficient way to tail the (stdout) log than readProcessStdoutLog(). Use
+     * readProcessStdoutLog() to read chunks and tailProcessStdoutLog() to tail.
+     *
+     * Requests (length) bytes from the (name)’s log, starting at (offset). If the total log size is
+     * greater than (offset + length), the overflow flag is set and the (offset) is automatically
+     * increased to position the buffer at the end of the log. If less than (length) bytes are
+     * available, the maximum number of available bytes will be returned. (offset) returned is
+     * always the last offset in the log +1.
+     *
+     * @param name   the name of the process (or ‘group:name’)
+     * @param offset offset to start reading from
+     * @param length maximum number of bytes to return
+     * @return array result [string bytes, int offset, bool overflow]
+     */
+    public boolean tailProcessStdoutLog(String name, int offset, int length) {
+        return true;
+    }
+
+    /**
+     * Provides a more efficient way to tail the (stderr) log than readProcessStderrLog(). Use
+     * readProcessStderrLog() to read chunks and tailProcessStderrLog() to tail.
+     *
+     * Requests (length) bytes from the (name)’s log, starting at (offset). If the total log size is
+     * greater than (offset + length), the overflow flag is set and the (offset) is automatically
+     * increased to position the buffer at the end of the log. If less than (length) bytes are
+     * available, the maximum number of available bytes will be returned. (offset) returned is
+     * always the last offset in the log +1.
+     *
+     * @param name   the name of the process (or ‘group:name’)
+     * @param offset offset to start reading from
+     * @param length maximum number of bytes to return
+     * @return array result [string bytes, int offset, bool overflow]
+     */
+    public boolean tailProcessStderrLog(String name, int offset, int length) {
+        return true;
+    }
+
+    /**
+     * Clear the stdout and stderr logs for the named process and reopen them.
+     *
+     * @param name The name of the process (or ‘group:name’)
+     * @return result Always True unless error
+     */
+    public boolean clearProcessLogs(String name) {
+        return true;
+    }
+
+    /**
+     * Clear all process log files
+     */
+    public boolean clearAllProcessLogs() {
+        return true;
+    }
+
+
 }
--- src/main/java/com/satikey/tools/supervisord/Supervisord.java
+++ src/main/java/com/satikey/tools/supervisord/Supervisord.java
@@ -10,6 +10,92 @@ public class Supervisord {
         return new Supervisord();
     }
 
+    /**
+     * Return the version of the RPC API used by supervisord
+     *
+     * @return string version version id
+     */
+    String getAPIVersion() {
+        return null;
+    }
+
+    /**
+     * Return the version of the supervisor package in use by supervisord
+     *
+     * @return string version version id
+     */
+    String getSupervisorVersion() {
+        return null;
+    }
+
+    /**
+     * Return identifying string of supervisord
+     *
+     * @return string identifier identifying string
+     */
+    public String getIdentification() {
+        return null;
+    }
+
+    /**
+     * {'statecode': 1, 'statename': 'RUNNING'} Return current state of supervisord as a struct
+     *
+     * @return struct A struct with keys int statecode, string statename
+     */
+    public String getState() {
+        return null;
+
+    }
+
+    /**
+     * Return the PID of supervisord
+     *
+     * @return int PID
+     */
+    public int getPID() {
+        return -1;
+    }
+
+    /**
+     * Read length bytes from the main log starting at offset
+     *
+     * @param offset offset to start reading from
+     * @param length number of bytes to read from the log.
+     * @return string result Bytes of log*
+     * @see http://supervisord.org/api.html#supervisor.rpcinterface.SupervisorNamespaceRPCInterface.readLog
+     */
+    public String readLog(int offset, int length) {
+        return null;
+    }
+
+    /**
+     * Clear the main log.
+     *
+     * @return boolean result always returns True unless error
+     */
+    public boolean clearLog() {
+        return true;
+    }
+
+    /**
+     * Shut down the supervisor process
+     *
+     * @return boolean result always returns True unless error
+     */
+    public boolean shutdown() {
+        return true;
+    }
+
+    /**
+     * Restart the supervisor process
+     *
+     * @return boolean result always return True unless error
+     */
+    public boolean restart() {
+        return false;
+    }
+
+    /////
     void listMethods() {
     }
 
